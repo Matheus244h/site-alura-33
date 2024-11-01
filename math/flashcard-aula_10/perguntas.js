50abcadc8180dbf88ced89104df02886c72d0d9b
Matheus244h <162708119+Matheus244h@users.noreply.github.com>
2024-11-01 11:05:08 -0300

    Add files via upload

---
+++ math/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'conhecimentos gerais',
+    'Quem foi a primeira pessoa a viajar no espaço',
+    'Yuri Gagarin'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Qual montanha mais alta do mundo?',
+    'Monte Everest'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Que país tem oformato de uma bota',
+    'Itália'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'quem inventou a lâmpada?',
+    'Thomas Edison'
+)
